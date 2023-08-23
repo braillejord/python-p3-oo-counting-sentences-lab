68a9c5181de7c6134dc5c0cc0514e77ab0718e14
BreElle Wells <breellejordyn@gmail.com>
2023-08-23 15:49:36 -0600

    create class and is_sentence method

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=" "):
+        self.value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    value = property(get_value, set_value)
+
+    def is_sentence(self):
+        return True if self.value.endswith(".") else False
